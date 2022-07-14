7b9cca8525a6598a5087ccd210f0ed34e5de0cd0
z2851david <105223657+z2851david@users.noreply.github.com>
2022-07-14 11:26:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,37 @@ def main_body():
     flags=pygame.SCALED
     running=True
     FPS=60
+    SCALE_T=1000  #1000 ticks for 1 second
+    SCALE_M=100  #100 pixels for every 1 meter
     class Projectile:
-        def __init__(self,object_name,initial_vel):
-            self.object_name=object_name
-            self.initial_vel=initial_vel
+        G=9.80665 #ms^-2
+        def __init__(self,initial_vel):
+            self.initial_vel=initial_vel   #ms^-1
+        def calc_velocity(self):
+            final_vel=math.sqrt((self.initial_vel*self.initial_vel)+2*Projectile.G*(HEIGHT/100))
+            print(final_vel)
+            return final_vel
+
+    class Circle(Projectile):
+        def __init__(self,initial_vel,center):
+            super().__init__(initial_vel)
+            self.center=center
+        def draw_circle(self):
+            final_vel=self.calc_velocity()
+            final_vel=final_vel
+            self.center[1]+=final_vel
+            pygame.draw.circle(WIN,"red",self.center,30)
+
+
+    circle1 = Circle(0, [500, 30])
 
 
     while running:
-        pygame.time.Clock().tick(framerate=FPS)
+        pygame.time.Clock().tick(FPS)
         WIN=pygame.display.set_mode((WIDTH, HEIGHT),flags)
-        pygame.draw.polygon(WIN,RED,points=[(100,600),(300,500),(800,800)],width=1)
+        circle1.draw_circle()
         pygame.display.flip()
+
 
 
 
