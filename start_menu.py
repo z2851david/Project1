31af898e4660cd34f532113d33d6696b618f7c34
z2851david <105223657+z2851david@users.noreply.github.com>
2022-08-30 22:09:40 +0100

    Update start_menu.py

--- start_menu.py
+++ start_menu.py
@@ -6,6 +6,7 @@ pygame.init()
 #----------------------test values----------------------#
 
 def menu1(WIN,WIDTH,HEIGHT,font):
+    WIN.fill("gray")
     text = font.render("Choose a motion", True, "green")
     textrect = text.get_rect()
     textrect.center = (WIDTH // 2, HEIGHT // 6)
@@ -28,37 +29,37 @@ def menu1(WIN,WIDTH,HEIGHT,font):
             if (WIDTH//2)-(text2.get_width()//2 ) <= mouse[0] <= (WIDTH//2)+(text2.get_width()//2) and\
                 (HEIGHT//2.7)-(text2.get_height()//2)<= mouse[1] <=(HEIGHT//2.7)+(text2.get_height()//2):
 
-                pygame.draw.rect(WIN,"white",text2rect)
+                pygame.draw.rect(WIN,"black",text2rect)
                 if events.type==pygame.MOUSEBUTTONDOWN:
                     setting_motion="vertical"
                     menu1_on=False
                     menu2_on=True
                     return setting_motion,menu1_on,menu2_on
             else:
-                pygame.draw.rect(WIN,"black",text2rect)
+                pygame.draw.rect(WIN,"gray",text2rect)
 
 
             if (WIDTH//2)-(text3.get_width()//2 ) <= mouse[0] <= (WIDTH//2)+(text3.get_width()//2) and\
                 (HEIGHT//1.3)-(text3.get_height()//2)<= mouse[1] <=(HEIGHT//1.3)+(text3.get_height()//2):
 
-                pygame.draw.rect(WIN,"white",text3rect)
+                pygame.draw.rect(WIN,"black",text3rect)
                 if events.type==pygame.MOUSEBUTTONDOWN:
                     quit()
             else:
-                pygame.draw.rect(WIN,"black",text3rect)
+                pygame.draw.rect(WIN,"gray",text3rect)
 
             if (WIDTH // 2) - (text4.get_width() // 2) <= mouse[0] <= (WIDTH // 2) + (text4.get_width() // 2) and \
                     (HEIGHT // 1.8) - (text4.get_height() // 2) <= mouse[1] <= (HEIGHT // 1.8) + (
                     text4.get_height() // 2):
 
-                pygame.draw.rect(WIN, "white", text4rect)
+                pygame.draw.rect(WIN, "black", text4rect)
                 if events.type == pygame.MOUSEBUTTONDOWN:
                     setting_motion="horizontal"
                     menu1_on=False
                     menu2_on=True
                     return setting_motion,menu1_on,menu2_on
             else:
-                pygame.draw.rect(WIN, "black", text4rect)
+                pygame.draw.rect(WIN, "gray", text4rect)
 
         WIN.blit(text,textrect)
         WIN.blit(text2,text2rect)
@@ -70,6 +71,7 @@ def menu1(WIN,WIDTH,HEIGHT,font):
 
 
 def menu2(WIN,WIDTH,HEIGHT,font):
+    WIN.fill("gray")
     text = font.render("Choose a projectile", True, "blue")
     textrect=text.get_rect()
     textrect.center=(WIDTH//2,HEIGHT//5)
@@ -89,27 +91,27 @@ def menu2(WIN,WIDTH,HEIGHT,font):
             if (WIDTH // 2) - (text2.get_width() // 2) <= mouse[0] <= (WIDTH // 2) + (text2.get_width() // 2) and \
                     (HEIGHT // 2) - (text2.get_height() // 2) <= mouse[1] <= (HEIGHT // 2) + (text2.get_height() // 2):
 
-                pygame.draw.rect(WIN, "white", text2rect)
+                pygame.draw.rect(WIN, "black", text2rect)
                 if events.type == pygame.MOUSEBUTTONDOWN:
                     setting_projectile = "circle"
                     menu2_on=False
                     menu1_on=False
                     return setting_projectile,menu1_on,menu2_on
             else:
-                pygame.draw.rect(WIN, "black", text2rect)
+                pygame.draw.rect(WIN, "gray", text2rect)
 
 
             if (WIDTH//2)-(text3.get_width()//2 ) <= mouse[0] <= (WIDTH//2)+(text3.get_width()//2) and\
                 (HEIGHT//1.2)-(text3.get_height()//2)<= mouse[1] <=(HEIGHT//1.2)+(text3.get_height()//2):
 
-                pygame.draw.rect(WIN,"white",text3rect)
+                pygame.draw.rect(WIN,"black",text3rect)
                 if events.type==pygame.MOUSEBUTTONDOWN:
                     setting_projectile=""
                     menu1_on=True
                     menu2_on=False
                     return setting_projectile,menu1_on,menu2_on
             else:
-                pygame.draw.rect(WIN,"black",text3rect)
+                pygame.draw.rect(WIN,"gray",text3rect)
 
         WIN.blit(text, textrect)
         WIN.blit(text2, text2rect)
