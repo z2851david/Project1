779de4811cd81ba0b5a781cc557b70386b866711
z2851david <105223657+z2851david@users.noreply.github.com>
2022-10-02 00:26:54 +0100

    Update projectiles.py

--- projectiles.py
+++ projectiles.py
@@ -16,10 +16,10 @@ class Projectile:
     def __init__(self,vel,angle, y_pos, x_pos):
         self.initial_vel=vel
         self.vel_before_motion=self.initial_vel
-        self.final_vel=0
+        self.vertical_vel=0
         self.angle = math.radians(angle)
         self.horizontal_vel = self.initial_vel * math.cos(self.angle)
-        self.vertical_vel = self.initial_vel * math.sin(self.angle)
+        self.old_vel = self.initial_vel * math.sin(self.angle)
         self.y_pos = y_pos
         self.x_pos = x_pos
         self.stop_y_motion=False
@@ -27,18 +27,19 @@ class Projectile:
         self.initial_vel=initial_vel
         self.angle=math.radians(angle)
         self.horizontal_vel = self.initial_vel * math.cos(self.angle)
-        self.vertical_vel = self.initial_vel * math.sin(self.angle)
+        self.old_vel = self.initial_vel * math.sin(self.angle)
+
+    def __del__(self):
+        return
 
 
 
-    def horizontal_motion(self,FPS,SCALE):
-        horizontal_displacement = (self.horizontal_vel / FPS) * SCALE
+    def motion(self,setting_object):
+        self.horizontal_vel = self.horizontal_vel
         if self.stop_y_motion==False:
-            self.final_vel = (-self.G * 1 / FPS) + (self.vertical_vel)  # suvat equation for final velocity
-        vertical_displacement = (self.vertical_vel * 1 / FPS) + ((-self.G) / 2 * (1 / FPS) * (1 / FPS))
-        vertical_displacement = vertical_displacement * SCALE  # distance to move objects across a tick/millisecond
-        self.vertical_vel = self.final_vel
-        return vertical_displacement,horizontal_displacement
+            self.vertical_vel = (-self.G*setting_object.delta_time) + (self.old_vel)
+        self.old_vel=self.vertical_vel
+        return self.vertical_vel,self.horizontal_vel
 
 
 class Ball(Projectile):
@@ -46,61 +47,72 @@ class Ball(Projectile):
         super().__init__(initial_vel,initial_angle,y_pos,x_pos)
         self.radius=radius
         self.center=center
-def initiate_circle(setting_motion):
+def initiate_circle(setting_motion,setting_obj):
 
     if setting_motion=="vertical":
-        red_circle=Ball(10,[WIDTH/2,0],0,0,10,WIDTH//2)
+        red_circle=Ball(20,[setting_obj.vertical_width/2,0],0,0,10,setting_obj.vertical_width//2)
     elif setting_motion=="horizontal":
-        red_circle=Ball(10,[10,HEIGHT-10],22,22,HEIGHT-10,10)
+        red_circle=Ball(20,[10,setting_obj.window_height//1.3],35,60,setting_obj.window_height//1.3-20,10)
     return red_circle
 
 
 
+def move_circle(red_circle,WIN,setting_obj):
+    vertical_velocity,horizontal_velocity=red_circle.motion(setting_obj)
+    red_circle.y_pos=red_circle.y_pos-(vertical_velocity*setting_obj.delta_time)*setting_obj.scale
+    red_circle.x_pos=red_circle.x_pos+(horizontal_velocity *setting_obj.delta_time)*setting_obj.scale
 
-def move_circle(red_circle,FPS,SCALE,WIN):
-    vertical_displacement,horizontal_displacement=red_circle.horizontal_motion(FPS,SCALE)
-    red_circle.y_pos=red_circle.y_pos-vertical_displacement
-    red_circle.x_pos=red_circle.x_pos+horizontal_displacement
+    if red_circle.y_pos > setting_obj.window_height//1.3-red_circle.radius:  # if ball goes over border
+        red_circle.y_pos = setting_obj.window_height//1.3-red_circle.radius #go back same amount
+        red_circle.vertical_vel=0
+        red_circle.horizontal_vel=0
+        red_circle.stop_y_motion=True
 
-    if red_circle.x_pos>= WIDTH:
-        red_circle.horizontal_vel=-red_circle.horizontal_vel  #if ball goes over right-border
+    if red_circle.x_pos >= setting_obj.setting_side_width:
+        red_circle.x_pos -= red_circle.horizontal_vel  # if ball goes over right-border
+        red_circle.horizontal_vel=0
 
-    if red_circle.y_pos + red_circle.radius > HEIGHT:  # if ball goes over border
-        red_circle.y_pos = red_circle.y_pos + vertical_displacement #go back same amount
-        if red_circle.horizontal_vel>0 and red_circle.stop_y_motion!=True:
-            red_circle.horizontal_vel-=3
-            if red_circle.horizontal_vel<0:
-                red_circle.horizontal_vel=0
+    if setting_obj.bounce==True:
 
-        if red_circle.horizontal_vel>0 and red_circle.stop_y_motion==True:
+        if red_circle.x_pos >= setting_obj.setting_side_width:
+            red_circle.horizontal_vel = -red_circle.horizontal_vel  # if ball goes over right-border
 
-            red_circle.horizontal_vel-=0.01
-            if red_circle.horizontal_vel<0:
-                red_circle.horizontal_vel=0
+            if red_circle.horizontal_vel>0 and red_circle.stop_y_motion!=True:
+                red_circle.horizontal_vel-=3
+                if red_circle.horizontal_vel<0:
+                    red_circle.horizontal_vel=0
 
-        if red_circle.horizontal_vel<0 and red_circle.stop_y_motion!=True:
-            red_circle.horizontal_vel+=3
-            if red_circle.horizontal_vel>0:
-                red_circle.horizontal_vel=0
+            if red_circle.horizontal_vel>0 and red_circle.stop_y_motion==True:
 
-        if red_circle.horizontal_vel<0 and red_circle.stop_y_motion==True:
-            red_circle.horizontal_vel+=0.01
-            if red_circle.horizontal_vel>0:
-                red_circle.horizontal_vel=0
+                red_circle.horizontal_vel-=0.01
+                if red_circle.horizontal_vel<0:
+                    red_circle.horizontal_vel=0
+
+            if red_circle.horizontal_vel<0 and red_circle.stop_y_motion!=True:
+                red_circle.horizontal_vel+=3
+                if red_circle.horizontal_vel>0:
+                    red_circle.horizontal_vel=0
+
+            if red_circle.horizontal_vel<0 and red_circle.stop_y_motion==True:
+                red_circle.horizontal_vel+=0.01
+                if red_circle.horizontal_vel>0:
+                    red_circle.horizontal_vel=0
 
 
-        red_circle.vertical_vel+=vertical_displacement
-        red_circle.vertical_vel+=3
-        red_circle.vertical_vel = -red_circle.vertical_vel
-        if red_circle.vertical_vel == -abs(red_circle.vertical_vel):  #if vertical velocity is negative
-            red_circle.stop_y_motion=True
-            red_circle.vertical_vel=0
-            pygame.draw.circle(WIN, "red", (red_circle.x_pos, red_circle.y_pos), red_circle.radius, 0)
+            red_circle.vertical_vel+=vertical_velocity
+            red_circle.vertical_vel+=3
+            red_circle.vertical_vel = -red_circle.vertical_vel
+            if red_circle.vertical_vel == -abs(red_circle.vertical_vel):  #if vertical velocity is negative
+                red_circle.stop_y_motion=True
+                red_circle.vertical_vel=0
+                pygame.draw.circle(WIN, "red", (red_circle.x_pos, red_circle.y_pos), red_circle.radius, 0)
 
-    if red_circle.x_pos<= 0:
-        red_circle.horizontal_vel=-red_circle.horizontal_vel
+            if red_circle.x_pos<= 0:
+                red_circle.horizontal_vel=-red_circle.horizontal_vel
 
 
     else:
         pygame.draw.circle(WIN,"red",(red_circle.x_pos,red_circle.y_pos),red_circle.radius,0)
+
+
 
