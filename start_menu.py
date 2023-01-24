a45765d3cd9ce351258aba8788bb154d54cad9db
z2851david <105223657+z2851david@users.noreply.github.com>
2023-01-24 20:00:28 +0000

    Update start_menu.py

--- start_menu.py
+++ start_menu.py
@@ -1,46 +1,40 @@
 import pygame
-#-----------------------test values---------------------#
-WIDTH, HEIGHT = 400, 400
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.init()
-#----------------------test values----------------------#
+import login_page
 
-def menu1(WIN,WIDTH,HEIGHT,font):
+def menu1(WIN,setting_obj,font):
+    pygame.init()
     WIN.fill("black")
-    text = font.render("Choose a motion", True, "turquoise")
+    text = font.render("Welcome to the simulation", True, "turquoise")
     textrect = text.get_rect()
-    textrect.center = (WIDTH // 2, HEIGHT // 6)
-    text2=font.render("Vertical",True,"turquoise")
+    textrect.center = (setting_obj.window_width // 2, setting_obj.window_height// 6)
+    text2=font.render("Start",True,"turquoise")
     text2rect=text2.get_rect()
-    text2rect.center=(WIDTH//2,HEIGHT//2.7)
+    text2rect.center=(setting_obj.window_width//2,setting_obj.window_height//2.7)
     text3=font.render("Exit",True,"turquoise")
     text3rect=text3.get_rect()
-    text3rect.center=(WIDTH//2,HEIGHT/1.3)
-    text4=font.render("Horizontal",True,"turquoise")
-    text4rect=text4.get_rect()
-    text4rect.center=(WIDTH//2,HEIGHT/1.8)
+    text3rect.center=(setting_obj.window_width//2,setting_obj.window_height//1.7)
 
 
-    while True:
+
+    while not setting_obj.close_main1:
         mouse=pygame.mouse.get_pos()
         for events in pygame.event.get():
             if events.type==pygame.QUIT:
                 quit()
-            if (WIDTH//2)-(text2.get_width()//2 ) <= mouse[0] <= (WIDTH//2)+(text2.get_width()//2) and\
-                (HEIGHT//2.7)-(text2.get_height()//2)<= mouse[1] <=(HEIGHT//2.7)+(text2.get_height()//2):
+            if (setting_obj.window_width //2)-(text2.get_width()//2 ) <= mouse[0] <= (setting_obj.window_width //2)+(text2.get_width()//2) and\
+                (setting_obj.window_height //2.7)-(text2.get_height()//2)<= mouse[1] <=(setting_obj.window_height //2.7)+(text2.get_height()//2):
 
                 pygame.draw.rect(WIN,"white",text2rect)
                 if events.type==pygame.MOUSEBUTTONDOWN:
-                    setting_motion="vertical"
-                    menu1_on=False
-                    menu2_on=True
-                    return setting_motion,menu1_on,menu2_on
+                     login_page.main_page(setting_obj)
+
+
             else:
                 pygame.draw.rect(WIN,"black",text2rect)
 
 
-            if (WIDTH//2)-(text3.get_width()//2 ) <= mouse[0] <= (WIDTH//2)+(text3.get_width()//2) and\
-                (HEIGHT//1.3)-(text3.get_height()//2)<= mouse[1] <=(HEIGHT//1.3)+(text3.get_height()//2):
+            if (setting_obj.window_width //2)-(text3.get_width()//2 ) <= mouse[0] <= (setting_obj.window_width //2)+(text3.get_width()//2) and\
+                (setting_obj.window_height //1.7)-(text3.get_height()//2)<= mouse[1] <=(setting_obj.window_height //1.7)+(text3.get_height()//2):
 
                 pygame.draw.rect(WIN,"red",text3rect)
                 if events.type==pygame.MOUSEBUTTONDOWN:
@@ -48,39 +42,28 @@ def menu1(WIN,WIDTH,HEIGHT,font):
             else:
                 pygame.draw.rect(WIN,"black",text3rect)
 
-            if (WIDTH // 2) - (text4.get_width() // 2) <= mouse[0] <= (WIDTH // 2) + (text4.get_width() // 2) and \
-                    (HEIGHT // 1.8) - (text4.get_height() // 2) <= mouse[1] <= (HEIGHT // 1.8) + (
-                    text4.get_height() // 2):
-
-                pygame.draw.rect(WIN, "white", text4rect)
-                if events.type == pygame.MOUSEBUTTONDOWN:
-                    setting_motion="horizontal"
-                    menu1_on=False
-                    menu2_on=True
-                    return setting_motion,menu1_on,menu2_on
-            else:
-                pygame.draw.rect(WIN, "black", text4rect)
-
         WIN.blit(text,textrect)
         WIN.blit(text2,text2rect)
         WIN.blit(text3,text3rect)
-        WIN.blit(text4,text4rect)
         pygame.display.update()
 
 
 
 
-def menu2(WIN,WIDTH,HEIGHT,font):
+def menu2(WIN,setting_obj,font):
     WIN.fill("black")
     text = font.render("Choose a projectile", True, "turquoise")
     textrect=text.get_rect()
-    textrect.center=(WIDTH//2,HEIGHT//5)
-    text2=font.render("Circle",True,"turquoise")
+    textrect.center=(setting_obj.window_width //2,setting_obj.window_height //5)
+    text2=font.render("Horizontal",True,"turquoise")
     text2rect=text2.get_rect()
-    text2rect.center=(WIDTH//2,HEIGHT//2)
+    text2rect.center=(setting_obj.window_width //2,setting_obj.window_height //1.7)
     text3=font.render("Go back",True,"turquoise")
     text3rect=text3.get_rect()
-    text3rect.center=(WIDTH//2,HEIGHT/1.2)
+    text3rect.center=(setting_obj.window_width //2,setting_obj.window_height //1.2)
+    text4=font.render("Vertical",True,"turquoise")
+    text4rect=text4.get_rect()
+    text4rect.center=(setting_obj.window_width //2,setting_obj.window_height //2.6)
 
 
     while True:
@@ -88,35 +71,49 @@ def menu2(WIN,WIDTH,HEIGHT,font):
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 quit()
-            if (WIDTH // 2) - (text2.get_width() // 2) <= mouse[0] <= (WIDTH // 2) + (text2.get_width() // 2) and \
-                    (HEIGHT // 2) - (text2.get_height() // 2) <= mouse[1] <= (HEIGHT // 2) + (text2.get_height() // 2):
+            if (setting_obj.window_width // 2) - (text2.get_width() // 2) <= mouse[0] <= (setting_obj.window_width // 2) + (text2.get_width() // 2) and \
+                    (setting_obj.window_height // 1.7) - (text2.get_height() // 2) <= mouse[1] <= (setting_obj.window_height // 1.7) + (text2.get_height() // 2):
 
                 pygame.draw.rect(WIN, "white", text2rect)
                 if events.type == pygame.MOUSEBUTTONDOWN:
-                    setting_projectile = "circle"
-                    menu2_on=False
-                    menu1_on=False
-                    return setting_projectile,menu1_on,menu2_on
+                    setting_motion = "horizontal"
+                    setting_obj.menu2_on=False
+                    setting_obj.menu1_on=False
+                    return setting_motion
             else:
                 pygame.draw.rect(WIN, "black", text2rect)
 
 
-            if (WIDTH//2)-(text3.get_width()//2 ) <= mouse[0] <= (WIDTH//2)+(text3.get_width()//2) and\
-                (HEIGHT//1.2)-(text3.get_height()//2)<= mouse[1] <=(HEIGHT//1.2)+(text3.get_height()//2):
+            if (setting_obj.window_width //2)-(text3.get_width()//2 ) <= mouse[0] <= (setting_obj.window_width //2)+(text3.get_width()//2) and\
+                (setting_obj.window_height //1.2)-(text3.get_height()//2)<= mouse[1] <=(setting_obj.window_height //1.2)+(text3.get_height()//2):
 
                 pygame.draw.rect(WIN,"red",text3rect)
                 if events.type==pygame.MOUSEBUTTONDOWN:
-                    setting_projectile=""
-                    menu1_on=True
-                    menu2_on=False
-                    return setting_projectile,menu1_on,menu2_on
+                    setting_motion=""
+                    setting_obj.menu1_on=True
+                    setting_obj.menu2_on=False
+                    setting_obj.close_main1=False
+                    return setting_motion
             else:
                 pygame.draw.rect(WIN,"black",text3rect)
+
+
+            if (setting_obj.window_width // 2) - (text4.get_width() // 2) <= mouse[0] <= (setting_obj.window_width // 2) + (text4.get_width() // 2) and \
+                    (setting_obj.window_height // 2.6) - (text4.get_height() // 2) <= mouse[1] <= (setting_obj.window_height // 2.6) + (text4.get_height() // 2):
+
+                pygame.draw.rect(WIN, "white", text4rect)
+                if events.type == pygame.MOUSEBUTTONDOWN:
+                    setting_motion = "vertical"
+                    setting_obj.menu2_on=False
+                    setting_obj.menu1_on=False
+                    return setting_motion
+            else:
+                pygame.draw.rect(WIN, "black", text4rect)
+
 
         WIN.blit(text, textrect)
         WIN.blit(text2, text2rect)
         WIN.blit(text3,text3rect)
+        WIN.blit(text4,text4rect)
         pygame.display.update()
 
-if __name__=="__main__":
-    menu2(WIN,WIDTH,HEIGHT)
